789e180370bc55174120ee6c5cd9ed64edc58fb8
edsenabr <ed@edsena.com.br>
2021-03-28 20:54:24 -0300

    initial release

--- VolumeSlider.js
+++ VolumeSlider.js
@@ -8,28 +8,37 @@ var VOLUME_ADJUSTMENT_STEP = 0.04; /* Volume adjustment step in % */
 
 
 class VolumeSlider extends PopupMenu.PopupSliderMenuItem {
-	constructor(applet, stream, tooltip, app_icon) {
+	constructor(magneticOn, volumeMax, volumeNorm, tooltip, app_icon) {
 			super(0);
-			this.applet = applet;
+			this.magneticOn = magneticOn;
+			this.volumeMax = volumeMax;
+			this.volumeNorm = volumeNorm;
 
-			if(tooltip)
-					this.tooltipText = tooltip + ": ";
-			else
-					this.tooltipText = "";
+			this.setTooltipText(tooltip);
+			this.set_mark(volumeNorm / volumeMax);
 
-			this.tooltip = new Tooltips.Tooltip(this.actor, this.tooltipText);
 
-			this.connect("value-changed", () => this._onValueChanged());
-
-			this.app_icon = app_icon;
-			this.icon = new St.Icon({icon_name: this.app_icon, icon_type: St.IconType.SYMBOLIC, icon_size: 16});
+			// this.icon = new St.Icon({icon_name: app_icon, icon_type: St.IconType.SYMBOLIC, icon_size: 16});
+			this.icon = new St.Icon({icon_name: app_icon, icon_type: St.IconType.SYMBOLIC, style_class: 'popup-menu-icon'});
 
 			this.removeActor(this._slider);
 			this.addActor(this.icon, {span: 0});
 			this.addActor(this._slider, {span: -1, expand: true});
 	}
 
+	setTooltipText(text){
+		if(!this.tooltip) {
+			this.tooltip = new Tooltips.Tooltip(this.actor, "");
+			this.tooltipText = (text) ?
+				`${text}: `:
+				""
+			;
+		}
+		this.tooltip.set_text(`${this.tooltipText}${text}`);
+	}
+
 	connectWithStream(stream) {
+		// global.log(`connectWithStream::init`);
 		this.actor.show();
 		this.stream = stream;
 		this.isMic = stream instanceof Cvc.MixerSource || stream instanceof Cvc.MixerSourceOutput;
@@ -42,127 +51,91 @@ class VolumeSlider extends PopupMenu.PopupSliderMenuItem {
 				stream.disconnect(volumeId);
 		});
 		this._update();
+		// global.log(`connectWithStream::done`);
 	}
 
-	_onValueChanged() {
-			if (!this.stream) return;
+	_changeValue(step) {
+		let value = this._value + step/this.volumeMax*this.volumeNorm;
+		value = (step < 0) ?
+			Math.max(0, value):
+			Math.min(1, value)
+		;
+		this._slider.queue_repaint();
 
-			let muted;
-			// Use the scaled volume max only for the main output
-			// let volume = this._value * (this.isOutputSink ? this.applet._volumeMax : this.applet._volumeNorm);
-			let volume = this._value * this.applet._volumeMax;
+		let muted;
+		let volume = value * this.volumeMax;
 
-			if(this._value < 0.005) {
-					volume = 0;
-					muted = true;
-			} else {
-					muted = false;
-					//100% is magnetic:
-					if (this.applet.magneticOn === true && volume != this.applet._volumeNorm && volume > this.applet._volumeNorm*(1-VOLUME_ADJUSTMENT_STEP/2) && volume < this.applet._volumeNorm*(1+VOLUME_ADJUSTMENT_STEP/2))
-							volume = this.applet._volumeNorm;
-			}
-			this.stream.volume = volume;
-			this.stream.push_volume();
+		if(value < 0.005) {
+				volume = 0;
+				muted = true;
+		} else {
+				muted = false;
+				//100% is magnetic:
+				if (this.magneticOn === true && volume != this.volumeNorm && volume > this.volumeNorm*(1-VOLUME_ADJUSTMENT_STEP/2) && volume < this.volumeNorm*(1+VOLUME_ADJUSTMENT_STEP/2))
+						volume = this.volumeNorm;
+		}
+		this.stream.volume = volume;
+		this.stream.push_volume();
 
-			if(this.stream.is_muted !== muted)
-					this.stream.change_is_muted(muted);
-
-			if(!this._dragging)
-					this.applet._notifyVolumeChange(this.stream);
+		// mute eh responsabilidade do applet
+		if(this.stream.is_muted !== muted) {
+			this.emit("toggle-mute", muted);
+		}
 	}
 
 	_onScrollEvent(actor, event) {
-			let direction = event.get_scroll_direction();
-
-			if (direction == Clutter.ScrollDirection.DOWN) {
-					this._value = Math.max(0, this._value - VOLUME_ADJUSTMENT_STEP/this.applet._volumeMax*this.applet._volumeNorm);
-			}
-			else if (direction == Clutter.ScrollDirection.UP) {
-					this._value = Math.min(1, this._value + VOLUME_ADJUSTMENT_STEP/this.applet._volumeMax*this.applet._volumeNorm);
-			}
-
-			this._slider.queue_repaint();
-			this.emit('value-changed', this._value);
+		global.log(`slider._onScrollEvent:: ${actor}`);
+			let step = 
+				(event.get_scroll_direction() == Clutter.ScrollDirection.UP) ?
+				VOLUME_ADJUSTMENT_STEP:
+				-VOLUME_ADJUSTMENT_STEP
+			;
+			this._changeValue(step);
 	}
 
 	_onKeyPressEvent(actor, event) {
-			let key = event.get_key_symbol();
-			if (key == Clutter.KEY_Right ||
-					key == Clutter.KEY_Left ||
-					key == Clutter.KEY_AudioRaiseVolume ||
-					key == Clutter.KEY_AudioLowerVolume)
-			{
-					let delta = (key == Clutter.KEY_Right || key == Clutter.KEY_AudioRaiseVolume) ? VOLUME_ADJUSTMENT_STEP : -VOLUME_ADJUSTMENT_STEP;
-					if (delta < 0) {
-							this._value = Math.max(0, this._value + delta/this.applet._volumeMax*this.applet._volumeNorm);
-					} else {
-							this._value = Math.min(1, this._value + delta/this.applet._volumeMax*this.applet._volumeNorm);
-					}
-					this._slider.queue_repaint();
-					this.emit('value-changed', this._value);
-					this.emit('drag-end');
-					return true;
-			}
-			return false;
-	}
+		let key = event.get_key_symbol();
+		let STEP = VOLUME_ADJUSTMENT_STEP/2;
+		switch(key) {
+			case Clutter.KEY_Left:
+			case Clutter.KEY_AudioLowerVolume:
+				STEP = -STEP;
+			case Clutter.KEY_Right:
+			case Clutter.KEY_AudioRaiseVolume:
+				break;
 
+			default:
+				return false;
+		}
+		this._changeValue(STEP);
+		this.emit('drag-end');
+		return true;
+	}
 
 	_update() {
-		global.log(`_update::init`);
-			// value: percentage of volume_max (set as value in the widget)
-			// visible_value: percentage of volume_norm (shown to the user)
-			// these only differ for the output, and only when the user changes the maximum volume
-			let volume = (!this.stream || this.stream.is_muted) ? 0 : this.stream.volume;
-			let value, visible_value, delta = VOLUME_ADJUSTMENT_STEP * this.applet._volumeMax / this.applet._volumeNorm;
-			global.logError(`_update::volume:${volume}`);
-			global.logError(`_update::value:${value}`);
-			global.logError(`_update::visible_value:${visible_value}`);
-			global.logError(`_update::delta:${delta}`);
-			global.logError(`_update::VOLUME_ADJUSTMENT_STEP:${VOLUME_ADJUSTMENT_STEP}`);
-			global.logError(`_update::_volumeMax::${this.applet._volumeMax}`);
-			global.logError(`_update::_volumeNorm::${this.applet._volumeNorm}`);
+		// global.log(`_update::init`);
+		// value: percentage of volume_max (set as value in the widget)
+		// visible_value: percentage of volume_norm (shown to the user)
+		// these only differ for the output, and only when the user changes the maximum volume
+		let volume = (!this.stream || this.stream.is_muted) ? 0 : this.stream.volume;
+		let value, visible_value, delta = VOLUME_ADJUSTMENT_STEP * this.volumeMax / this.volumeNorm;
 
-			value = volume / this.applet._volumeMax;
-			visible_value = volume / this.applet._volumeNorm;
-			if (this.applet.magneticOn === true && visible_value != 1 && visible_value > 1 - delta/2 && visible_value < 1 + delta/2) {
-					visible_value = 1; // 100% is magnetic
-					value = this.applet._volumeNorm / this.applet._volumeMax;
-					// this.applet._output.volume = this.applet._volumeNorm;
-					// this.applet._output.push_volume();
-			}
-
-			let percentage = Math.round(visible_value * 100) + "%";
-
-			this.tooltip.set_text(this.tooltipText + percentage);
-			if (this._dragging)
-					this.tooltip.show();
-			let iconName = this._volumeToIcon(value);
-			if (this.app_icon == null) {
-					this.icon.icon_name = iconName;
-			}
-			this.setValue(value);
-			// send data to applet
-			this.emit("values-changed", iconName, percentage);
-			global.log(`_update::done`);
-
+		value = volume / this.volumeMax;
+		visible_value = volume / this.volumeNorm;
+		if (this.magneticOn === true && visible_value != 1 && visible_value > 1 - delta/2 && visible_value < 1 + delta/2) {
+				visible_value = 1; // 100% is magnetic
+				value = this.volumeNorm / this.volumeMax;
 		}
 
-	_volumeToIcon(value) {
-			let icon;
-			if(value < 0.005) {
-					icon = "muted";
-			} else {
-					let n = Math.floor(3 * value);
-					if(n < 1)
-							icon = "low";
-					else if(n < 2)
-							icon = "medium";
-					else
-							icon = "high";
-			}
-			return this.isMic? "microphone-sensitivity-" + icon : "audio-volume-" + icon;
+		let percentage = Math.round(visible_value * 100) + "%";
+		this.setTooltipText(percentage);
+		this.emit("volume-changed", percentage);
+
+		if (this._dragging) {
+			this.tooltip.show();
+		}
+		this.setValue(value);
+		// global.log(`_update::done`);
 	}
 }
-
-
 module.exports = {VolumeSlider}
--- applet.js
+++ applet.js
@@ -6,6 +6,7 @@ const Main = imports.ui.main;
 const Lang = imports.lang;
 const PopupMenu = imports.ui.popupMenu;
 const St = imports.gi.St;
+const Util = imports.misc.util;
 const {VolumeSlider} = require('./VolumeSlider');
 const {MultiIconApplet} = require('./MultiIconApplet');
 
@@ -16,71 +17,93 @@ const VOLUME_ADJUSTMENT_STEP = 0.04; /* Volume adjustment step in % */
 const setup = {
 	SPEAKERS: {
 		"_output": "::Auto-Falantes com echo-canceling",
-		"_input": "::Boya BY-MM1"
+		"_input": "::Boya BY-MM1",
+		"name": "Speakers",
+		"label": "Auto-Falantes + Boya BY-MM1"
 	},
 	HEADSET: {
 		"_output": "Plantronics Blackwire 5220 Series::Headphones",
-		"_input": "Plantronics Blackwire 5220 Series::Headset Microphone"
+		"_input": "Plantronics Blackwire 5220 Series::Headset Microphone",
+		"name": "Headset",
+		"label": "Plantronics Blackwire 5220 Series",
 	}
 };
 
 
 class SimpleSoundApplet extends MultiIconApplet {
-
-
 	constructor(orientation, panelHeight, instanceId) {
 		super(orientation, panelHeight, instanceId);
 		this.set_applet_icon_symbolic_name('_input','audio-input-microphone-symbolic');
 		this.set_applet_icon_symbolic_name('_output','audio-headset-symbolic');
-		this.set_applet_tooltip("Switches between devices");
 		this.mute_switch = [];
 		this.mute_id = [];
-		this.device = [];
+		this.stream = [];
 		this.volume_selection = [];
 		this.devices = {
 			"_output": [],
 			"_input": []
 		};
 
-		this.setupControls();
+		this.setupMixerControls();
 		this.drawMenu(orientation);
+		this.setupListeners();
+
+		this._setKeybinding();
 	}
 
 	drawMenu(orientation) {
+		// controles
+		this.mute_switch['_output'] = new PopupMenu.PopupSwitchIconMenuItem(_("Mute output"), false, "audio-headset-symbolic", St.IconType.SYMBOLIC);
+		this.mute_switch['_input'] = new PopupMenu.PopupSwitchIconMenuItem(_("Mute input"), false, "audio-input-microphone-symbolic", St.IconType.SYMBOLIC);
+		this.volume_selection['_output'] = new VolumeSlider(true, this._volumeMax, this._volumeNorm, _("Volume"), 'audio-headset-symbolic');
+		this.volume_selection['_input'] = new VolumeSlider(true, this._volumeMax, this._volumeNorm, _("Microphone"), 'audio-input-microphone-symbolic');
+
+		// menu
 		this.menuManager = new PopupMenu.PopupMenuManager(this);
 		this.menu = new Applet.AppletPopupMenu(this, orientation);
 		this.menuManager.addMenu(this.menu);
 
-		this.mute_switch['_output'] = new PopupMenu.PopupSwitchIconMenuItem(_("Mute output"), false, "audio-headset-symbolic", St.IconType.SYMBOLIC);
-		this.mute_switch['_input'] = new PopupMenu.PopupSwitchIconMenuItem(_("Mute input"), false, "audio-input-microphone-symbolic", St.IconType.SYMBOLIC);
-		this.mute_switch['_output'].connect('toggled', () => this._toggle_mute('_output'));
-		this.mute_switch['_input'].connect('toggled', () => this._toggle_mute('_input'));
-
-		this.menu.addMenuItem(this.mute_switch['_output']);
+		// botoes de mute
 		this.menu.addMenuItem(this.mute_switch['_input']);
+		this.menu.addMenuItem(this.mute_switch['_output']);
+
 		this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
 
+		this.menu.addMenuItem(this._drawDevice(setup.HEADSET));
+		this.menu.addMenuItem(this._drawDevice(setup.SPEAKERS));
 
-		this.volume_selection['_output'] = new VolumeSlider(this, this._control.get_default_sink(), _("Volume"), 'audio-headset-symbolic');
-		this.volume_selection['_input'] = new VolumeSlider(this, this._control.get_default_source(), _("Microphone"), 'audio-input-microphone-symbolic');
+		this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
 
-		// this.volume_selection['_output'].connect("values-changed", (...args) => this._inputValuesChanged(...args));
-		// this.volume_selection['_input'].connect("values-changed", (...args) => this._outputValuesChanged(...args));
-
-		// this._inputVolumeSection.connect("values-changed", (...args) => this._inputValuesChanged(...args));
-		// this._outputVolumeSection.connect("values-changed", (...args) => this._outputValuesChanged(...args));
-
+		// deslizantes de volume
 		this.menu.addMenuItem(this.volume_selection['_input']);
 		this.menu.addMenuItem(this.volume_selection['_output']);
 
 		this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-		this.menu.addSettingsAction(_("Sound Settings"), 'sound');
-		this.menu.addSettingsAction(_("PA Settings"), 'pavucontrol.desktop');
 
-		
+		// ajustes
+		this.menu.addSettingsAction(_("Sound Settings"), 'sound');
+		this.menu.addCommandlineAction(_("Pulse Audio Settings"), 'pavucontrol');
+
 	}
 
-	setupControls() {
+	_drawDevice(setup){
+		let device = new PopupMenu.PopupMenuItem(setup.name);
+		let bin = new St.Bin({ x_align: St.Align.END, style_class: 'popup-active-menu-item' });
+		// let label = new St.Label({ text: setup.label });
+		// bin.add_actor(label);
+		device.addActor(bin, { expand: false, span: -1, align: St.Align.END });
+		device.activate = () => this._toggle_setup(setup);
+		setup.control = device;
+		return device;
+	}
+
+	_toggle_setup(setup) {
+		global.log(`_toggle_setup:: ${setup.name}`);
+		this._control.change_input(this.devices['_input'][setup['_input']]);
+		this._control.change_output(this.devices['_output'][setup['_output']]);
+	}
+
+	setupMixerControls() {
 		Main.keybindingManager.addHotKey("sound-open-" + this.instance_id, this.keyOpen, Lang.bind(this, this.on_applet_clicked));
 		this._control = new Cvc.MixerControl({ name: 'Cinnamon Volume Control' });
 		this._sound_settings = new Gio.Settings({ schema_id: CINNAMON_DESKTOP_SOUNDS });
@@ -97,134 +120,52 @@ class SimpleSoundApplet extends MultiIconApplet {
 		this._control.connect('state-changed', (...args) => this._onControlStateChanged(...args));
 		this._control.connect('active-output-update', (...args) => this._onDeviceUpdate(...args, "_output"));
 		this._control.connect('active-input-update', (...args) => this._onDeviceUpdate(...args, "_input"));
-
-
-		this.actor.connect('scroll-event', (...args) => this._onScrollEvent(...args));
-		// this.actor.connect('key-press-event', (...args) => this._onKeyPressEvent(...args));
-
-
 	}
 
-// 	_valuesChanged(type) {
-// 		this.mute_switch[type]
-// 		this.mute_in_switch.setIconSymbolicName(iconName);
-// 	}
+	setupListeners() {
+		this.mute_switch['_output'].connect('toggled', () => this._toggle_mute('_output'));
+		this.mute_switch['_input'].connect('toggled', () => this._toggle_mute('_input'));
+		this.volume_selection['_output'].connect("volume-changed", (...args) => this._updateTooltip(...args));
+		this.volume_selection['_input'].connect("toggle-mute", () => this._toggle_mute('_input'));
+		this.volume_selection['_output'].connect("toggle-mute", () => this._toggle_mute('_output'));
+		this.actor.connect('scroll-event', (...args) => this.volume_selection['_output']._onScrollEvent(...args));
+	}
 
-// 	_outputValuesChanged(actor, iconName, percentage) {
-// 		this.setIcon(iconName, "output");
-// 		this.mute_out_switch.setIconSymbolicName(iconName);
-// 		this.set_applet_tooltip(_("Volume") + ": " + percentage);
+	_setKeybinding() {
+		Main.keybindingManager.addHotKey("toggle-mute-" + this.instance_id, "Pause", Lang.bind(this, () => this._toggle_mute('_input')));
+		Main.keybindingManager.addHotKey("use-headset-" + this.instance_id, "<Super>h", Lang.bind(this, () => this._toggle_setup(setup.HEADSET)));
+		Main.keybindingManager.addHotKey("use-speakers-" + this.instance_id, "<Super>s", Lang.bind(this, () => this._toggle_setup(setup.SPEAKERS)));
+	}
 
-// 		if (this.defaultColor === null) {
-// 				//this.actor.set_style("applet-box");
-// 				let themeNode = this.actor.get_theme_node();
-// 				this.defaultColor = themeNode.get_foreground_color();
-// 		}
-// 		let color = "rgba("+this.defaultColor.red+","+this.defaultColor.green+","+this.defaultColor.blue+","+this.defaultColor.alpha+")";
-
-// 		if (this.adaptColor) {
-// 				let pc = Math.round(percentage.split("%")[0]);
-// 				if (pc > 130)
-// 						color = "red";
-// 				else if (pc > 115)
-// 						color = "orange";
-// 				else if (pc > 100)
-// 						color = "yellow";
-// 		}
-// 		let _style = "color: %s;".format(color);
-// 		this.actor.style = _style;
-// 		this._outputVolumeSection.icon.style = _style;
-// 		this._outputVolumeSection.style = _style;
-// }
-
-// _inputValuesChanged(actor, iconName) {
-// 		this.mute_in_switch.setIconSymbolicName(iconName);
-// }
+	on_applet_removed_from_panel () {
+		Main.keybindingManager.removeHotKey("toggle-mute-" + this.instance_id);
+		Main.keybindingManager.removeHotKey("use-headset-" + this.instance_id);
+		Main.keybindingManager.removeHotKey("use-speakers-" + this.instance_id);
+	}
 
 
-
-	// _set_default_device(device) {
-	// 	let output = device['_output'];
-	// 	let input = device['_input'];
-	// 	global.log(`changing default output to: ${output}`);
-	// 	global.log(`changing default input to: ${input}`);
-
-
-	// 	let sink = this.devices['_output'][output];
-	// 	let source = this.devices['_input'][input];
-	// 	global.log(`changing default to: ${sink.description} and ${source.description}`);
-	// 	this._control.set_default_sink(sink);
-	// 	global.log("1");
-	// 	this._control.set_default_source(source);
-	// 	global.log("2");
-	// }
-
-	_onScrollEvent(actor, event) {
-		let direction = event.get_scroll_direction();
-		let currentVolume = this.device['_output'].volume;
-		global.log(`_onScrollEvent:: ${direction}`);
-
-		if (direction == Clutter.ScrollDirection.DOWN) {
-			global.log(`_onScrollEvent:: down`);
-			let prev_muted = this.device['_output'].is_muted;
-				this.device['_output'].volume = Math.max(0, currentVolume - this._volumeNorm * VOLUME_ADJUSTMENT_STEP);
-				if (this.device['_output'].volume < 1) {
-						this.device['_output'].volume = 0;
-						if (!prev_muted)
-								this.device['_output'].change_is_muted(true);
-				} else {
-						// 100% is magnetic:
-						if (this.magneticOn === true && this.device['_output'].volume!=this._volumeNorm && this.device['_output'].volume>this._volumeNorm*(1-VOLUME_ADJUSTMENT_STEP/2) && this.device['_output'].volume<this._volumeNorm*(1+VOLUME_ADJUSTMENT_STEP/2))
-								this.device['_output'].volume=this._volumeNorm;
-				}
-				this.device['_output'].push_volume();
-		}
-		else if (direction == Clutter.ScrollDirection.UP) {
-			global.log(`_onScrollEvent:: uo`);
-				this.device['_output'].volume = Math.min(this._volumeMax, currentVolume + this._volumeNorm * VOLUME_ADJUSTMENT_STEP);
-				// 100% is magnetic:
-				if (this.magneticOn === true && this.device['_output'].volume!=this._volumeNorm && this.device['_output'].volume>this._volumeNorm*(1-VOLUME_ADJUSTMENT_STEP/2) && this.device['_output'].volume<this._volumeNorm*(1+VOLUME_ADJUSTMENT_STEP/2))
-						this.device['_output'].volume=this._volumeNorm;
-				this.device['_output'].push_volume();
-				this.device['_output'].change_is_muted(false);
-		}
-		// this._notifyVolumeChange(this.device['_output']);
-		let percentage = ((this.device['_output'].volume/this._volumeNorm).toFixed(2)*100).toFixed(0);
-		this.set_applet_tooltip(_("Volume") + ": " + percentage + "%");
-	}	
-
+	_updateTooltip(actor, percentage) {
+		this.set_applet_tooltip(_("Volume") + ": " + percentage);
+	}
+	
 	_onButtonPressEvent (actor, event) {
-		switch (event.get_button())  {
-			case 9:
-				this._set_default_device(setup.HEADSET);
-				break;
-			;;
-
-			case 8:
-				this._set_default_device(setup.SPEAKERS);
-				break;
-			;;
-
-			case 2:
-				this._toggle_mute('_input');
-				break;
-				// trocar device
-			;;
-
-			default:
-				return Applet.Applet.prototype._onButtonPressEvent.call(this, actor, event);
-			
+		if (event.get_button() == 2) {
+			this._toggle_mute('_input');
+			return Clutter.EVENT_STOP;
 		}
-		return Clutter.EVENT_STOP;
+		return Applet.Applet.prototype._onButtonPressEvent.call(this, actor, event);
 	}
 
 	_toggle_mute(property) {
-		if (!this.device[property])
+		global.log(`_toggle_mute:: ${property}`)
+		if (!this.stream[property])
 				return;
-		this.device[property].change_is_muted(
-			!this.device[property].is_muted
+
+		this.stream[property].change_is_muted(
+			!this.stream[property].is_muted
 		);
-		this.mute_switch[property].setToggleState(this.device[property].is_muted);
+		this.mute_switch[property].setToggleState(this.stream[property].is_muted);
+		global.log(`_toggle_mute:: done`)
 	}
 
 	_onDeviceAdded(control, id, type) {
@@ -238,56 +179,55 @@ class SimpleSoundApplet extends MultiIconApplet {
 	_onDeviceUpdate(control, id, type) {
 		let device = this._control[`lookup${type}_id`](id);
 		let stream =  this._control.get_stream_from_device(device);
-		// global.logError(`_onDeviceUpdate:: (${type}) ${device.origin}:: ${device.description}`);
+		this.stream[type] = stream;
+		global.log(`_onDeviceUpdate:: ${type} ${device.origin}::${device.description} is ${stream.is_muted}`);
 
-		let icon = 
-			(device.origin == 'Plantronics Blackwire 5220 Series') ?
-			"audio-headset-symbolic":
-			"audio-speakers-symbolic"
-		;
+
+		if (this.mute_id[type]) {
+			this.stream[type].disconnect(this.mute_id[type]);
+			this.mute_id[type] = 0;
+		}
+
+		if (this.stream[type]) {
+			this.mute_id[type] = this.stream[type].connect('notify::is-muted', () => this._onMutedChanged(type));
+		}
+
+		let icon = '';
+		let active = null;
+		let inactive = null;
+		if (device.origin == 'Plantronics Blackwire 5220 Series') {
+			icon = "audio-headset-symbolic";
+			control  = setup.HEADSET.control;
+			inactive = setup.SPEAKERS.control;
+		} else {
+			icon = "audio-speakers-symbolic"
+			control  = setup.SPEAKERS.control;
+			inactive = setup.HEADSET.control;
+		}
+		control.setShowDot(true);
+		inactive.setShowDot(false);
 		this.set_applet_icon_symbolic_name('_output', icon);
 		this.mute_switch['_output'].setIconSymbolicName(icon);
+		this.volume_selection['_output'].icon.set_icon_name(icon);
 		this.volume_selection[type].connectWithStream(stream);
+		this._onMutedChanged (type);
 	}
 
 	_onControlStateChanged() {
-		// global.log('_onControlStateChanged::');
 		if (this._control.get_state() == Cvc.MixerControlState.READY) {
-				this._read('_output');
-				this._read('_input');
-				let percentage = ((this.device['_output'].volume/this._volumeNorm).toFixed(2)*100).toFixed(0);
-				this.set_applet_tooltip(_("Volume") + ": " + percentage + "%");
 				this.actor.show();
 		} else {
 			this.actor.hide();
 		}
 	}
 
-	_read(property) {
-		// disconnects existing listener
-		if (this.mute_id[property]) {
-			this.device[property].disconnect(this.mute_id[property]);
-			this.mute_id[property] = 0;
-		}
+	_onMutedChanged(property) {
+		global.log(`_mutedChanged::init (${property}) >> ${this.stream[property].is_muted}`)
 
-		this.device[property] = 
-			(property == '_input') ?
-			this._control.get_default_source():
-			this._control.get_default_sink()
-		;
-
-		if (this.device[property]) {
-			// this._inputVolumeSection.connectWithStream(this.device['_input']);
-			this.mute_id[property] = this.device[property].connect('notify::is-muted', (...args) => this._mutedChanged(...args, property));
-			this._mutedChanged (null, null, property);
-		}
-	}
-
-	_mutedChanged(object, param_spec, property) {
-		this.mute_switch[property].setToggleState(this.device[property].is_muted);
+		this.mute_switch[property].setToggleState(this.stream[property].is_muted);
 		let defaultColor = this.actor.get_theme_node().get_foreground_color();
-		this._applet_icons[property].style = 
-			this.device[property].is_muted ?
+		let style = 
+			this.stream[property].is_muted ?
 			"color: red;":
 			"color: (%s,%s,%s,%s);".format(
 				defaultColor.red,
@@ -296,6 +236,11 @@ class SimpleSoundApplet extends MultiIconApplet {
 				defaultColor.alpha
 			)
 		;
+
+		this._applet_icons[property].style = style;
+		this.volume_selection[property].icon.style = style;
+		global.log(`_mutedChanged::done`)
+
 	}
 
 	on_applet_clicked(event) {
@@ -307,5 +252,3 @@ class SimpleSoundApplet extends MultiIconApplet {
 function main(metadata, orientation, panelHeight, instanceId) {
 	return new SimpleSoundApplet(orientation, panelHeight, instanceId);
 }
-
-
