fa45061f2cc105ddc526e519e1f54b08ab77befb
Eduardo Sena S. Rosa <edsena@google.com>
2023-12-02 15:52:00 -0300

    ... adding log levels

--- LogUtils.js
+++ LogUtils.js
@@ -1,17 +1,32 @@
 class LogUtils {
 	compact = false;
+	static levels = {
+		Info: 1,
+		Warning: 2,
+		Error: 3,
+		Debug: 4
+	};
 
-	constructor(enabled){
-		this.enabled = (enabled !== false); //caring for undefined
+	constructor(level, name){
+		this._level = Number.isInteger(level) && 
+						level > 0 && 
+						level < 5
+					? level 
+					: LogUtils.levels.Info;
+		this._name = name;
 	}
 
 	init(...args) {
-		this.info(args);
+		this._log('Debug',...args)
 
 	}
 
+	done(...args) {
+		this._log('Debug',...args)
+	}
+
 	info(...args) {
-		this._log('', ...args)
+		this._log('Info', ...args)
 	}
 
 	warn (...args) {
@@ -23,25 +38,37 @@ class LogUtils {
 	}
 
 	debug(...args) {
-		this._log('DEBUG',...args)
+		this._log('Debug',...args)
 	}
 
-	_log(level, ...args) {
-		if (!this.enabled) return;
+	_getStack() {
 		let stack =	new Error()
 			.stack
 			.replaceAll(/(\/<)?\@.+$/gm, '')
 			.replaceAll(/main.+/gs,'');
 		if (LogUtils.compact) {
-			stack = stack.replaceAll(/(((?<!_)[a-z])|_)+/gs,'');
+			stack = stack.replace(/(((?<!_)[a-z])|_)+/gs,'');
 		}
-		
-		stack = stack.split(/\n/).slice(2).reverse().join('::');
-		if (args.length == 1){
-			global[`log${level == 'DEBUG' ? '' : level}`](`${level == 'DEBUG' ? 'DEBUG' : 'LOG'}${stack}::${args[0]}`);
-		} else {
-			global[`log${level == 'DEBUG' ? '' : level}`](`${level == 'DEBUG' ? 'DEBUG' : 'LOG'}${stack}::`, args);
+		stack = stack.replace(/.+\/</mg, 'anonymous')
+		return stack.split(/\n/).slice(2).reverse().join('::');
+	}
 
-		}
+
+	_log(level, ...args) {
+		level = LogUtils.levels[level];
+		if ( level > this._level) return;
+		let stack = this._getStack();
+		if (!stack.startsWith(`::${this._name}`))
+			stack = `::${this._name}${stack}`;
+		
+		let header = `${level == LogUtils.levels.Debug ? 'DEBUG' : 'LOG'}${stack}`;
+		let method = global[`log${[LogUtils.levels.Debug, LogUtils.levels.Info].includes(level) ? '' : level}`];
+		let message;
+		if (args.length == 0)
+			method(header);
+		else if (args.length == 1)
+			method(`${header}::${args[0]}`);
+		else
+			method(header, args);
 	}
 }
